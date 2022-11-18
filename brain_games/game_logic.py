fb7ad79bbec152716ef67b3bf8f317c5a9fc7174
gagushin2@gmail.com <gagushin2@gmail.com>
2022-11-18 12:07:31 +0400

    rebuild 1

--- brain_games/game_logic.py
+++ brain_games/game_logic.py
@@ -3,8 +3,9 @@ from brain_games import cli
 
 def run(game):
     cli.welcome()
-    print(game.DESCRIPTION)
     name = cli.get_name()
+    cli.greet(name)
+    print(game.DESCRIPTION)
     print()
     for _ in range(3):
         q, a = game.generate_qa_pair()
